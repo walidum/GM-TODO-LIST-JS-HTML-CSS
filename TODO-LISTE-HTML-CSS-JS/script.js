dedb30b7da72eb0afc33e64784dd591721ec37a5
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-04-09 15:59:26 +0100

    first commit

--- TODO-LISTE-HTML-CSS-JS/script.js
+++ TODO-LISTE-HTML-CSS-JS/script.js
@@ -28,41 +28,23 @@ function createNewNote() {
 
     var content = document.getElementById('id-content')
     content.appendChild(card)
+    dShow();
 }
 
-function createNote() {
-    var title = prompt('Entrer le titre de la note :')
-    var content = prompt('Entrer le contenu de la note :')
-
-    var card = document.createElement('div')
-    card.setAttribute('class', 'card')
-
-    var head = document.createElement('div')
-    head.setAttribute('class', 'head')
-    card.appendChild(head)
-
-    var headspan = document.createElement('span')
-    headspan.innerHTML = title
-    head.appendChild(headspan)
-
-    var headimg = document.createElement('img')
-    headimg.setAttribute('src', 'images/delete.png')
-    headimg.setAttribute('onclick', 'remove(this)')
-    head.appendChild(headimg)
-
-    var text = document.createElement('div')
-    text.setAttribute('class', 'text')
-    card.appendChild(text)
-
-    var spantext = document.createElement('span')
-    spantext.innerHTML = content
-    text.appendChild(spantext)
-
-    var content = document.getElementById('id-content')
-    content.appendChild(card)
-}
 
 function remove(el) {
     var toRemove = el.parentNode.parentNode
     toRemove.remove()
+}
+
+function show() {
+    document.getElementById('form')
+        .style.display = 'flex'
+    document.getElementById('id-content')
+        .style.display = 'none'
+}
+
+function dShow() {
+    document.getElementById('form').style.display = 'none'
+    document.getElementById('id-content').style.display = 'flex'
 }
