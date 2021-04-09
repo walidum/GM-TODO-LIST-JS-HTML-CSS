4624900bb9cd835d55680ef0171161e4ce18e7c1
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-04-09 15:41:54 +0100

    first commit

--- TODO-LISTE-HTML-CSS-JS/script.js
+++ TODO-LISTE-HTML-CSS-JS/script.js
@@ -1,3 +1,35 @@
+function createNewNote() {
+    var title = document.getElementById('title').value
+    var content = document.getElementById('content').value
+
+    var card = document.createElement('div')
+    card.setAttribute('class', 'card')
+
+    var head = document.createElement('div')
+    head.setAttribute('class', 'head')
+    card.appendChild(head)
+
+    var headspan = document.createElement('span')
+    headspan.innerHTML = title
+    head.appendChild(headspan)
+
+    var headimg = document.createElement('img')
+    headimg.setAttribute('src', 'images/delete.png')
+    headimg.setAttribute('onclick', 'remove(this)')
+    head.appendChild(headimg)
+
+    var text = document.createElement('div')
+    text.setAttribute('class', 'text')
+    card.appendChild(text)
+
+    var spantext = document.createElement('span')
+    spantext.innerHTML = content
+    text.appendChild(spantext)
+
+    var content = document.getElementById('id-content')
+    content.appendChild(card)
+}
+
 function createNote() {
     var title = prompt('Entrer le titre de la note :')
     var content = prompt('Entrer le contenu de la note :')
